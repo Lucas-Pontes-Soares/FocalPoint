c4697ff0138a04976c6bedbd806ab93f73d082d2
Lucas <eterneru.lucas@outlook.com>
2024-01-26 17:02:34 -0300

    Arrumando para data atual, seguindo o formato correto

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,14 +3,29 @@ import styles from '../styles/header.module.css';
 //pegando data atual
 const currentDate = new Date();
 
+const nomesDosMeses = [
+  'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
+  'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'
+];
+
 // ex: "Segunda, 22 de dezembro de 2024"
+
 const formattedDate = currentDate.toLocaleDateString('pt-BR', {
   weekday: 'long',
-  day: 'numeric',
-  month: 'long',
-  year: 'numeric',
 });
 
+const dia = currentDate.getDate();
+const ano = currentDate.getFullYear();
+const mesNumero = currentDate.getMonth();
+const mesNome = nomesDosMeses[mesNumero];
+
+// ex: segunda-feira para Segunda
+const weekday = formattedDate.split(',')[0].trim();
+const diaSemana = weekday.replace(/-/g, '').replace('feira', '');
+const diaSemanaMaisuculo = diaSemana.charAt(0).toUpperCase() + diaSemana.slice(1);
+
+// criando texto correto
+const dataAtual = `${diaSemanaMaisuculo}, ${dia} de ${mesNome} de ${ano}`
 
 export default function Header() {
   return (
@@ -22,7 +37,7 @@ export default function Header() {
             <h1 className={styles.welcome}>Bem-vindo de volta, Lucas</h1>
         </div>
         <div className={styles.column3}>
-            <p className={styles.dateText}>{formattedDate}</p>
+            <p className={styles.dateText}>{dataAtual}</p>
         </div>
     </div>
   );
